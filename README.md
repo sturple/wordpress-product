85d1f03bbf60fcff8b85b4dd0eb3c44a5d2a4c68
sturple <shawn.turple@shaw.ca>
2016-07-09 11:02:44 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wordpress Plugin (Add Product Data without an Ecommerce Plugin)
+
+
---
+++ wordpress-product.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Wordpress Social
+ * Plugin URI: https://github.com/sturple/wordpress-social/
+ * Description: Wordpress plugin to add social intigration for twitter and facebook
+ * Version: 0.0.1
+ * Author: Shawn Turple / Robert Leahy
+ * Author URI: http://turple.ca
+ * License: GPL-3.0
+ */
+
+if ( file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' ) /* check in self */
+	|| file_exists( $composer_autoload = WP_CONTENT_DIR.'/vendor/autoload.php') /* check in wp-content */
+	|| file_exists( $composer_autoload = plugin_dir_path( __FILE__ ).'vendor/autoload.php') /* check in plugin directory */
+	|| file_exists( $composer_autoload = get_stylesheet_directory().'/vendor/autoload.php') /* check in child theme */
+	|| file_exists( $composer_autoload = get_template_directory().'/vendor/autoload.php') /* check in parent theme */
+) {
+	require_once $composer_autoload;
+}
