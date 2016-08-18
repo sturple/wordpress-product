c8e06596891e05cafa33e86f9d2eac34f2320d57
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-08-18 03:04:08 -0700

    Specifications Field Group/Repeater

--- parts/meta-boxes/features.php
+++ parts/meta-boxes/features.php
@@ -9,3 +9,24 @@ piklist('field',[
     'field' => 'features',
     'label' => __('Features')
 ]);
+
+piklist('field',[
+    'type' => 'group',
+    'field' => 'specifications',
+    'label' => __('Specifications'),
+    'add_more' => true,
+    'fields' => [
+        [
+            'type' => 'text',
+            'label' => __('Name'),
+            'field' => 'specification-name',
+            'columns' => 12
+        ],
+        [
+            'type' => 'text',
+            'label' => __('Description'),
+            'field' => 'specification-description',
+            'columns' => 12
+        ]
+    ]
+]);
